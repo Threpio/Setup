68efcfe9d0726659caa75e4cae830a51e23efae0
Threpio <49570057+Threpio@users.noreply.github.com>
2019-07-22 11:59:55 +0100

    Updated Ansible-Comp_Setup to include DNF Update.
    Added DNF update code to ensure correct downloads.

--- Ansible-Comp_setup.sh
+++ Ansible-Comp_setup.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 # Attempting to BASH script some ansible into my life!
+# This script has to be run as the ROOT user.
 
-# Assuming Root Access
-# sudo su
+# Checking if DNF is up to date:
+echo STEP: Updating DNF
+dnf check-update
 
 # Install Git and Ansible
 echo STEP: Installing Git
@@ -11,7 +13,7 @@ dnf install -y git
 echo STEP: Installing Ansible
 dnf install -y ansible
 
-# Pulling ansible-playbook from GIT
+# Pulling ansible-playbook from GIT  <= Why does this command not work?
 echo STEP: Pulling Git - Ansible File
 git pull https://github.com/Threpio/Setup.git
 
