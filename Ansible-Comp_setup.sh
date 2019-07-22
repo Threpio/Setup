1eae282c7566378cdb71301b0a8c3b81c01657d2
Threpio <49570057+Threpio@users.noreply.github.com>
2019-07-22 16:02:33 +0100

    Update Ansible-Comp_setup.sh
    Changed the initial Git command from Git Pull to Git Clone as a possible fix to function error.
    Not tested as not at Dev11.

--- Ansible-Comp_setup.sh
+++ Ansible-Comp_setup.sh
@@ -14,7 +14,9 @@ echo STEP: Installing Ansible
 dnf install -y ansible
 
 # Pulling ansible-playbook from GIT  <= Why does this command not work?
-echo STEP: Pulling Git - Ansible File
+echo STEP: Cloning Git - Ansible File
+git clone https://github.com/Threpio/Setup.git
+echo STEP: Pulling Git - Not sure if this step is neccessary
 git pull https://github.com/Threpio/Setup.git
 
 # Attempting to run Playbook <= Why does this command not work?
