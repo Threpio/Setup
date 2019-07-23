42be90d12948a86b9a4cb1486c235695697b45fa
Theo Andresier <49570057+Threpio@users.noreply.github.com>
2019-07-23 10:21:36 +0100

    Update Ansible-Comp_setup.sh

--- Ansible-Comp_setup.sh
+++ Ansible-Comp_setup.sh
@@ -3,19 +3,15 @@
 # Attempting to BASH script some ansible into my life!
 # This script has to be run as the ROOT user.
 
-# Checking if DNF is up to date:
-echo STEP: Updating DNF
-dnf install
-
 # Install Git and Ansible
-echo STEP: Installing Git
-dnf install -y git
-echo STEP: Installing Ansible
-dnf install -y ansible
+echo STEP: Installing Git and Ansible
+dnf install -y git ansible
 
 # Pulling ansible-playbook from GIT  <= Why does this command not work?
 echo STEP: Cloning Git - Ansible File
 git clone https://github.com/Threpio/Setup.git
 
 # Attempting to run Playbook <= Why does this command not work?
-ansible-playbook Setup/playbook.yaml
+cd Setup
+ansible-playbook playbook.yaml
+cd ..
